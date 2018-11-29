9ea69661dfcb563f5fc9bfc02fd8776c3118d2a5
nuriacasal <43879011+nuriacasal@users.noreply.github.com>
2018-11-29 09:54:36 +0100

    Add files via upload

---
+++ ejercicios propuestos/EdadPorPantalla/EdadPantalla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdadPantalla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM;
+            string linea;
+            Console.WriteLine("INGRESE SU EDAD EN AÑOS:");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            Console.WriteLine("Su Edad: " + NUM);
+            Console.WriteLine("Pulse una Tecla para salir del programa:");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ejercicios propuestos/FuncionesString/FuncionesString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cadena = ("Esto es una cadena/");
+            Console.Write(cadena);
+            String cadena2 =("y esta otra cadena de texto");
+            Console.WriteLine(cadena + cadena2);
+            char c2 = cadena[2];
+            Console.ReadLine();
+            Console.WriteLine(c2);
+            Console.ReadLine();
+            Console.WriteLine("Hola Mundo".IndexOf('M'));
+            Console.ReadLine();
+            Console.WriteLine(cadena.Length);
+            Console.ReadLine();
+            Console.Write(cadena.Substring(cadena.Length - 1, 1));
+            Console.ReadLine();
+            Console.WriteLine(cadena.Substring(2, 6));
+            Console.ReadLine();
+
+        }
+    }
+}
