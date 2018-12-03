fa05c933f5a39b0a9a8740ad62c6e97c94e22eb9
nuriacasal <43879011+nuriacasal@users.noreply.github.com>
2018-12-03 22:41:48 +0100

    3/12/18

---
+++ ejercicios propuestos/Clases/Clase1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    class Program
+    {
+        public class Persona
+        {
+            public const int totalpersonasensala = 12;
+            public String Nombre;
+            public String Apellido;
+        }
+
+
+        public static void Main()
+        {
+            Persona p1 = new Persona();
+            p1.Nombre = "Nuria";
+            p1.Apellido = "Casal";
+            Console.WriteLine("Su nombre es {0}, y su apellido es {1}", p1.Nombre, p1.Apellido);
+            Console.ReadLine();
+            Console.WriteLine("Ella estaba en la sala, junto a otras {0} personas", Persona.totalpersonasensala);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+					
