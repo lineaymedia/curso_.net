8f4ddab305b97d60003663a9a85b76f82fb6c84f
Daniel Canteli Martínez <danicant@gmail.com>
2018-12-04 00:48:50 +0100

    Ejercicio 4
    Lista de números primos hasta el 1000 (bucles)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace ListaNumerosPrimos
+
+{
+
+    class ListaNumerosPrimos
+
+    {
+
+        static void Main()
+
+        {
+
+            for (int i = 1; i <= 1000; i++)
+
+            {
+
+                if (i % 2 == 0 && i != 2 ) continue;
+                if (i % 3 == 0 && i != 3 ) continue;
+                if (i % 5 == 0 && i != 5 ) continue;
+                if (i % 7 == 0 && i != 7 ) continue;              
+
+                Console.WriteLine(i);
+                
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+
+    }
+
+}
+
