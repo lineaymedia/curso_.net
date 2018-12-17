caf6db60cbb62a7601f23badd54e5efa40a105df
danicant <danicant@gmail.com>
2018-12-18 00:43:07 +0100

    herencias_dcm

---
+++ ConsoleApp4/ConsoleApp4/Personas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    public class Personas
+    {
+        private string direccion;
+        private string nombre;
+        private string Direccion { get => direccion; set => direccion = value; }
+        private string Localidad { get => localidad; set => localidad = value; }
+        public string Nombre { get => Nombre; set => Nombre = value; }
+        public int Nif { get => Nif; set => Nif = value; }
+        public string Tipo { get => Tipo; set => Tipo = value; }
+
+        private int nif;
+        private string tipo;
+
+        private string localidad;
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empresa Voragine;
+            Voragine = new Empresa();
+            Voragine.Direccion = "Calle Profesor Potter, 183;
+            Voragine.Localidad = "Gijón";
+        }
+    }
+}
