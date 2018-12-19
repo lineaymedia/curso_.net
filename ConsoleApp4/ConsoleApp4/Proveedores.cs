6b16541e36098aa03a91e8bb1d44619386a1d215
danicant <danicant@gmail.com>
2018-12-20 00:06:53 +0100

    Ejercicio de herencias

---
+++ ConsoleApp4/ConsoleApp4/Proveedores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    public class Proveedores : Personas
+    {
+        private int codigoProveedor;
+
+        public int CodigoProveedor { get => codigoProveedor; set => codigoProveedor = value; }
+    }
+}
