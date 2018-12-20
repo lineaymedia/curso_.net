4c837018eabeefc6f19b9b318b18725ace46a119
javier-lineaymedia <javier@lineaymedia.com>
2018-12-20 08:26:31 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Persona
+    {
+        public string Nombre;
+        public int Edad;
+
+    }
+
+    class Alumno : Persona
+    {
+        public static void Main()
+         {
+            Alumno Alumno1 = new Alumno("Juan", 20);
+
+            public void ListaAlumnos()
+            {
+                Console.WriteLine("Alumno: " + Nombre + "Edad: " + Edad);
+
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
