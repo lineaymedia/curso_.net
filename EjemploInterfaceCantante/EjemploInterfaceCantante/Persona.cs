8f9e7e2ef51e5cdf0018acaf7c50e7fcee7ec494
mariatelenti <mariatelenti@gmail.com>
2019-01-15 11:18:03 +0100

    Ejemplos clase 12

---
+++ EjemploInterfaceCantante/EjemploInterfaceCantante/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploInterfaceCantante
+{
+	class Persona : ICantante
+	{
+		string nombre;
+		int edad;
+
+		public Persona(string nombre, int edad)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+		}
+
+		public string Imprimir()
+		{
+			return "Soy " + nombre + " y tengo " + edad + " años";
+		}
+
+		public string Cantar()
+		{
+			return "Asturias Patria Queridaaaa";
+		}
+	}
+}
---
+++ EjemploInterfaceCantante/EjemploInterfaceCantante/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploInterfaceCantante
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Persona p = new Persona("Juan", 30);
+			Canario c = new Canario();
+
+			Console.WriteLine("Juan canta: " + p.Cantar());
+			Console.WriteLine("El canario canta: " + c.Cantar());
+
+			ICantante[] cantantes = { p, c};
+			foreach (ICantante cnt in cantantes)
+			{
+				Console.WriteLine(cnt.Cantar());
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
